ef07c3cdac48a784e244ab1d520f0b33267cdf4f
Felipe Ferreira Menegueli <felipe.menegueli@t2mlab.com>
2024-09-23 22:06:55 -0300

    init javascript

---
+++ Serratec2024-2/FrontendEssencial/Aula-Javascript/script.js
@@ -0,0 +1,52 @@
+// C -> CREATE ->  -> RAFAEL FREI (ID =2)
+// R -> READ -> GET -> RAFAEL FREI, MARINA MAYUMI
+// U -> UPDATE -> POST / PUT -> ID=2 -> RAFAEL FREIRE
+// D -> DELETE -> DELETE -> RAFAEL FREIRE
+
+// R -> READ -> GET -> MARINA MAYUMI
+
+// fetch() -> Forma de fazer solicitações a uma API
+
+// var nome = 'felipe'
+// let sobreNome = 'menegueli'
+// const nome2 = 'ferreira'
+
+// console.log(nome);
+
+// console.log(sobreNome);
+
+// {
+//   console.log('Nome dentro do bloco de codigo')
+// }
+
+// Utilizando if e else
+// if(nome == 'ferreira') {
+//   console.log('O nome esta errado')
+// } else if(nome == 'menegueli') {
+//   console.log('O nome esta incorreto')
+// } else {
+//   console.log('o nome esta certo')
+// }
+
+// bloco de codigo para numeros pares
+{
+  console.log(2)
+  console.log(4)
+  console.log(6)
+  console.log(8)
+  console.log(10)
+}
+
+// bloco de codigo para numeros impares
+{
+  console.log(1)
+  console.log(3)
+  console.log(5)
+  console.log(7)
+  console.log(9)
+}
+// Janela do browser
+window
+
+// Documento do browser
+document
