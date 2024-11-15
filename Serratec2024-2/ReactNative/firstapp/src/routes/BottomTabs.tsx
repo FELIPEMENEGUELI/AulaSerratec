49378846d6a890406652c6440bace22b102d4262
Felipe Ferreira Menegueli <felipe.menegueli@t2mlab.com>
2024-11-14 21:18:37 -0300

    style bottom tabs

--- Serratec2024-2/ReactNative/firstapp/src/routes/BottomTabs.tsx
+++ Serratec2024-2/ReactNative/firstapp/src/routes/BottomTabs.tsx
@@ -5,6 +5,7 @@ import { Home } from '../screens/Home';
 import { Profile } from '../screens/Profile';
 import { Settings } from '../screens/Settings';
 import { useAuth } from '../hooks/useAuth';
+import { styles } from './style';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,57 +14,96 @@ export const TabRouters = () => {
   const { email } = useAuth();
 
   return (
-    <Tab.Navigator 
-      screenOptions={{ 
-          title: '',
-          headerBackground: () => (
-            <View style={{ 
-              backgroundColor: '#f0f', 
-              height: 80, 
-              justifyContent: 'center',
-              }}>
-              <Text style={{ color: '#FFF', fontWeight: 'bold', fontSize: 30 }}>
-                Seja bem vinda, {email ? email : "Faça login"}
-              </Text>
-            </View>
-          )
-        }}>
+    <Tab.Navigator
+      screenOptions={{
+        title: '',
+        headerBackground: () => (
+          <View style={{
+            backgroundColor: '#1B75BB',
+            height: 80,
+            justifyContent: 'center',
+          }}>
+            <Text style={{ color: '#FFF', fontWeight: 'bold', fontSize: 30 }}>
+              Seja bem vinda, {email ? email : "Faça login"}
+            </Text>
+          </View>
+        ),
+        tabBarStyle: {
+          backgroundColor: '#1b75bb',
+          height: 70
+        }
+      }}>
 
-      <Tab.Screen 
-        name="TabsHome" 
+      <Tab.Screen
+        name="TabsHome"
         component={Home}
         options={{
-          tabBarIcon: () => (
-            <View>
-              <Text>Home</Text>
-              <Ionicons name="home" size={24} color="black" />
+          tabBarIcon: ({ focused, size }) => (
+            <View style={styles.boxTabBar}>
+              <Ionicons name="home" size={focused ? 30 : size} color={focused ? '#fff' : '#999'} />
+              <Text
+                style={[styles.textTabRoute,
+                {
+                  color: focused ? '#fff' : '#999'
+                }
+                ]}>Home</Text>
             </View>
           )
         }}
       />
-      
+
       <Tab.Screen
-        name="TabsProfile" 
+        name="TabsProfile"
         component={Profile}
         options={{
-          tabBarIcon: () => (
-            <View>
-              <Text>Profile</Text>
-              <Ionicons name="person" size={24} color="black" />
+          tabBarIcon: ({ focused, size }) => (
+            <View style={styles.boxTabBar}>
+              <Ionicons name="person" size={focused ? 30 : size} color={focused ? '#fff' : '#999'} />
+              <Text
+                style={[styles.textTabRoute,
+                {
+                  color: focused ? '#fff' : '#999'
+                }
+                ]}
+              >Profile</Text>
             </View>
           )
         }}
       />
-      
-      <Tab.Screen 
-        name="TabsSettings" 
-        component={Settings} 
+
+      <Tab.Screen
+        name="TabsSettings"
+        component={Settings}
         options={{
           headerShown: false,
-          tabBarIcon: () => (
-            <View>
-              <Text>Settings</Text>
-              <Ionicons name="settings" size={24} color="black" />
+          tabBarIcon: ({ focused, size }) => (
+            <View style={styles.boxTabBar}>
+              <Ionicons name="settings" size={focused ? 30 : size} color={focused ? '#fff' : '#999'} />
+              <Text
+                style={[styles.textTabRoute,
+                {
+                  color: focused ? '#fff' : '#999'
+                }
+                ]}>Settings</Text>
+            </View>
+          )
+        }}
+      />
+
+      <Tab.Screen
+        name="TabsNotification"
+        component={Settings}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, size }) => (
+            <View style={styles.boxTabBar}>
+              <Ionicons name="alert-sharp" size={focused ? 30 : size} color={focused ? '#fff' : '#999'} />
+              <Text
+                style={[styles.textTabRoute,
+                {
+                  color: focused ? '#fff' : '#999'
+                }
+                ]}>Alert</Text>
             </View>
           )
         }}
---
+++ Serratec2024-2/ReactNative/firstapp/src/routes/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  boxTabBar: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+
+  textTabRoute: {
+    fontSize: 16,
+  }
+});
