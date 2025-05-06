7cb17aaff0db3175944134eaeaa223684b0ad0ab
Felipe Ferreira Menegueli <felipe.menegueli@t2mlab.com>
2025-05-06 20:49:59 -0300

    add fetch

--- Serratec-2025-1/FrontendEssencial/Aula_05/script.js
+++ Serratec-2025-1/FrontendEssencial/Aula_05/script.js
@@ -27,7 +27,7 @@ const pessoa = {
   nome: "Nikolas",
   idade: 20,
   nascimento: "01/01/2000",
-  saudacao: function() {
+  saudacao: function () {
     return "Ola " + this.nome
   }
 }
@@ -42,11 +42,11 @@ const pessoa = {
 
 // const somar = a, b = a+b
 
-function saudacao(a, b){
+function saudacao(a, b) {
   return a + b
 }
 
-  var alunos = [
+var alunos = [
   {
     id: 1,
     nome: "Bruno",
@@ -73,7 +73,7 @@ var novosAlunos = [
 
 // var newAlunos = alunos.push({nome: "Marlos", idade: 20})
 
-novosAlunos.push({nome: "Adriana", idade: 20})
+novosAlunos.push({ nome: "Adriana", idade: 20 })
 // console.log("Escreva: ", novosAlunos)
 
 const arrayTeste = [2, 3, 4, 5]
@@ -111,8 +111,8 @@ var usuarios = [
     estado: "Minnesota",
     role: 56,
     id: "2"
-    },
-    {
+  },
+  {
     name: "Mrs. Ruby Klocko",
     email: "Taryn_Emard@example.net",
     avatar: "Invalid faker method - image.people",
@@ -124,7 +124,7 @@ var usuarios = [
     estado: "West Virginia",
     role: 60,
     id: "3"
-    }
+  }
 ]
 
 const entrar = () => {
@@ -132,15 +132,165 @@ const entrar = () => {
   var senhaInput = document.getElementById("senhaUser").value;
 
   var resultadoEmailESenha = usuarios.find(
-    usuario => usuario.email === emailInput && 
-    usuario.password === senhaInput)
+    usuario => usuario.email === emailInput &&
+      usuario.password === senhaInput)
 
-  if(resultadoEmailESenha) {
-    console.log('Usuario existe', resultadoEmailESenha)
+  if (resultadoEmailESenha) {
+    // console.log('Usuario existe', resultadoEmailESenha)
   } else {
-    console.log('usuario nao existe', resultadoEmailESenha)
+    // console.log('usuario nao existe', resultadoEmailESenha)
   }
 }
+
+async function exemploVarivavel() {
+
+  if (true) {
+    let arrayLet = 'teste let'
+    var array = 'teste var'
+  }
+
+  // console.log('Uso variavel VAR', array)
+  // console.log('Uso variavel LET', arrayLet)
+}
+
+const nome = "Serratec"
+// console.log(nome.replace("Serratec", "Residencia TIC Software"))
+
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+
+const meuCarro = new Car("BMW", 2025)
+// console.log(`Meu carro é da marca ${meuCarro.name} e do ano ${meuCarro.year}`)
+
+
+var contador = 1
+// while(contador <= 10){
+//   console.log("Laço While", contador++)
+// }
+
+// do {
+//   console.log("Laço do-while", contador++)
+// } while (contador < 10);
+
+
+// for(var count = 1; count < 10; count++){
+//   console.log(`Laço for ${count}`)
+// }
+
+const arrayFrutas = [{ id: 1, nome: "Morango" }, { id: 2, nome: "Uva" }];
+
+// pega o objeto completo
+// for(let nomeVariavel of arrayFrutas) {
+//   console.log('Pegando o objeto do array', nomeVariavel)
+// }
+
+// for(let nomeVariavel in arrayFrutas) {
+//   console.log('Pegando o indice do array', nomeVariavel)
+// }
+
+// arrayFrutas.forEach(element => {
+//     console.log("Elemento", element)
+// });
+
+// setTimeout(() => {
+//   console.log("Aula frontend")
+// }, 5000);
+
+// setInterval(() => {
+//   console.log("Aula frontend")
+// }, 20000);
+
+
+// Envio pro backend
+// JSON.stringify => pega um objeto Javascript, transforma em JSON e envia pro back
+// const inputEmail = document.getElementById("emailUser").value;
+// const inputPassword = document.getElementById("passwordUser").value;
+
+// var objetoIndoProBack = [{ email: inputEmail, password: inputPassword }]
+// const convercaoObjetoParaJson = JSON.stringify(objetoIndoProBack);
+// console.log("Convertido para JSON", convercaoObjetoParaJson);
+// Convertido para JSON [{"email":"Uriel.Ward84@example.net","password":123}]
+
+// Chegada do backend
+// JSON.parse => pega uma string JSON e transforma em um objeto Javascript
+const jsonVindoDoBack = '[{ "email": "Uriel.Ward84@example.net", "password": 123 }]'
+const convercaoJsonParaObjeto = JSON.parse(jsonVindoDoBack)
+// console.log("Convertido para Objeto", convercaoJsonParaObjeto);
+// Convertido para Objeto { email: 'Uriel.Ward84@example.net', password: 123 }
+
+
+const entrando = () => {
+  var emailInput = document.getElementById("emailUser").value;
+  var senhaInput = document.getElementById("senhaUser").value;
+
+  var resultadoEmailESenha = usuarios.find(
+    usuario => usuario.email === emailInput &&
+      usuario.password === senhaInput)
+
+  if (resultadoEmailESenha) {
+    salvarLocalStorage(emailInput);
+    // console.log('Usuario existe', resultadoEmailESenha)
+  } else {
+    // console.log('usuario nao existe', resultadoEmailESenha)
+  }
+}
+
+const salvarLocalStorage = (emailInput) => {
+  localStorage.setItem("userEmail", emailInput)
+}
+
+const sairSistema = () => {
+  localStorage.clear();
+  localStorage.removeItem("userEmail")
+}
+
+const pegarValorLocalStorage = () => {
+  localStorage.getItem("userEmail");
+}
+
+async function buscarDados() {
+  console.log('Carregando...')
+  
+  const url = "https://66f4ad8277b5e889709a277e.mockapi.io/api/v1/users"
+  
+  try {
+      setTimeout(async () => {
+        const resposta = await fetch(url)
+        const dados = await resposta.json()
+        console.log("Exibindo", dados)
+        console.log('Carregou')
+      }, 2000)
+  } catch (erro) {
+    console.error("Erro ao buscar os dados:", erro)
+  }
+}
+
+// Requisição como serviço
+async function ServicoBuscarDados() {
+  const url = "https://66f4ad8277b5e889709a277e.mockapi.io/api/v1/users"
+  try {
+    const resposta = await fetch(url)
+    const dados = await resposta.json()
+    return dados;
+  } catch (erro) {
+    return erro;
+  }
+}
+
+const usandoOServico = async () => {
+  const resposta = await ServicoBuscarDados();
+  if(resposta.status === 200) {
+    console.log(resposta)
+  } else {
+    console.log('Error')
+  }
+}
+
+// buscarDados()
 
 // repositorio julya
 // integrantes
